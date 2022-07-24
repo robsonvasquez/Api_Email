60ac25fbd8db950ab0dbdfe2b82ef80c3ba3ac90
robson vasquez <ribeirovasquez@gmail.com>
2022-07-24 19:40:38 -0300

    Teste site online

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const config_email = nodemailer.createTransport({
 
 app.use(body_parser.json());
 
-app.use(cors());
+app.use(cors({origin: process.env.REQUIRE_URL}));
 
 app.post("/send-email", (req, res) => {
 
